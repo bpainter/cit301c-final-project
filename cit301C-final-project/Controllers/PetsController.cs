20caa8043489392c5445d53c96f348a34ba89a51
Williams-Nisha <nishamwilliams@yahoo.com>
2017-03-31 23:38:54 -0600

    Created Pet class, connected database, made edit, details and delete pages

---
+++ cit301C-final-project/Controllers/PetsController.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using cit301C_final_project.Models;
+
+namespace cit301C_final_project.Controllers
+{
+    public class PetsController : Controller
+    {
+        private PetDBContext db = new PetDBContext();
+
+        // GET: Pets
+        public ActionResult Index(string searchString)
+        {
+            var pets = from p in db.Pet
+                         select p;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                pets = pets.Where(s => s.AnimalCategory.Contains(searchString));
+            }
+
+            return View(pets);
+        }
+
+        // GET: Pets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pet.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // GET: Pets/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,SerialNum,AnimalCategory,AnimalName,BirthDate,Breed,OwnerName,OwnerStreet,OwnerState,OwnerZip,OwnerPhoneNum,PhotoUrl")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pet.Add(pet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(pet);
+        }
+
+        // GET: Pets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pet.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Pets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,SerialNum,AnimalCategory,AnimalName,BirthDate,Breed,OwnerName,OwnerStreet,OwnerState,OwnerZip,OwnerPhoneNum,PhotoUrl")] Pet pet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(pet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(pet);
+        }
+
+        // GET: Pets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Pet pet = db.Pet.Find(id);
+            if (pet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(pet);
+        }
+
+        // POST: Pets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Pet pet = db.Pet.Find(id);
+            db.Pet.Remove(pet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ cit301C-final-project/Models/Pet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace cit301C_final_project.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string SerialNum { get; set; }
+        public string AnimalCategory { get; set; }
+        public string AnimalName { get; set; }
+
+        [Display(Name = "Pet's Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:MM-dd-yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime BirthDate { get; set; }
+
+        public string Breed { get; set; }
+        public string OwnerName { get; set; }
+        public string OwnerStreet { get; set; }
+        public string OwnerState { get; set; }
+        public string OwnerZip { get; set; }
+        public string OwnerPhoneNum { get; set; }
+        public string PhotoUrl { get; set; }
+    }
+    public class PetDBContext : DbContext
+    {
+        public DbSet<Pet> Pet { get; set; }
+    }
+}
